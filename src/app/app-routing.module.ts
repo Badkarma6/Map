cab509aaa43c5b0ff5fbd3fe434a1dbb0cdba718
Angel <ahe1395@wgu.edu>
2024-03-25 14:07:12 -0500

    Update routing module and map display component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapDisplayComponent } from './map-display/map-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MapDisplayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
