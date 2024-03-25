7abcb4aaadbb8fd1be05d3c39a6d619edeefa47a
Angel <ahe1395@wgu.edu>
2024-03-25 17:02:01 -0500

    adding the country data layout

--- src/app/map-display/map-display.component.ts
+++ src/app/map-display/map-display.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-map-display',
-  templateUrl: './map-display.component.html',
-  styleUrl: './map-display.component.scss'
+  selector: 'app-map-display', // This is how you reference the component in HTML
+  templateUrl: './map-display.component.html', // Path to the HTML template
+  styleUrls: ['./map-display.component.scss'] // Corrected to be an array
 })
 export class MapDisplayComponent {
-
+  // Add this method inside your component class
+  onMapClick(event: MouseEvent): void {
+    console.log("Map clicked", event);
+  }
 }
+
