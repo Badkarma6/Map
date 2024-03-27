05678b00ec4aa27441bf3f1b4ea8940aa3caa9b7
Angel <ahe1395@wgu.edu>
2024-03-26 22:07:15 -0500

    edited the mapinfo from

--- src/app/geo-data.service.ts
+++ src/app/geo-data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeoDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCountryInformation(countryCode: string): Observable<any> {
+    const url = `http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&country=${countryCode}&username=D280AngelH&style=full`;
+    return this.http.get(url);
+  }
 }
--- src/app/map-display/map-display.component.ts
+++ src/app/map-display/map-display.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core'; // Added AfterViewInit
+import { GeoDataService } from '../services/geo-data.service';
 
 @Component({
-  selector: 'app-map-display', // This is how you reference the component in HTML
-  templateUrl: './map-display.component.html', // Path to the HTML template
-  styleUrls: ['./map-display.component.scss'] // Corrected to be an array
+  selector: 'app-map-display',
+  templateUrl: './map-display.component.html',
+  styleUrls: ['./map-display.component.scss']
 })
-export class MapDisplayComponent {
-  // Add this method inside your component class
+export class MapDisplayComponent implements AfterViewInit { // Implement AfterViewInit
+  constructor(private geoDataService: GeoDataService) { }
+
+  ngAfterViewInit(): void {
+    // Assuming your SVG is directly in the template. Adjust the selector if needed.
+    const svgElement: SVGElement = document.querySelector('svg');
+    const pathElements: NodeListOf<SVGPathElement> = svgElement.querySelectorAll('path');
+    pathElements.forEach((path: SVGPathElement) => {
+      path.addEventListener('click', (event: MouseEvent) => this.handleClick(event));
+    });
+  }
+
+  handleClick(event: MouseEvent) {
+    const path = event.target as SVGPathElement;
+    const countryCode = path.id; // Assuming the SVG path id is the country code
+
+    // Use the GeoDataService to fetch country information
+    this.geoDataService.getCountryInformation(countryCode).subscribe((data: any) => {
+      console.log(data); // For now, just log the data to see if it works
+    });
+  }
+
+  // Your existing onMapClick method, if still needed
   onMapClick(event: MouseEvent): void {
     console.log("Map clicked", event);
   }
 }
-
