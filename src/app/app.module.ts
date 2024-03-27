1359a18b9fe8bea2c91c050e506768d8d244ef84
Angel <ahe1395@wgu.edu>
2024-03-26 20:51:17 -0500

    Add HttpClientModule to AppModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldInformationComponent } from './world-information/world-information.component';
@@ -15,6 +15,7 @@ import { MapDisplayComponent } from './map-display/map-display.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
